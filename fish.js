2e0334e037e527eae7dbcf39aeb1ac85855fd4f3
marie077 <marieow35@gmail.com>
2024-07-19 00:44:08 -0400

    progress

---
+++ fish.js
@@ -0,0 +1,9 @@
+class Fish {
+    constructor(x, y, xSpeed, ySpeed) {
+      this.x = x;
+      this.y = y;
+      this.xSpeed = xSpeed;
+      this.ySpeed = ySpeed;
+    }
+  }
+  
--- sketch.js
+++ sketch.js
@@ -1,14 +1,20 @@
-let bg;
+let bg, fish1;
 let square;
+let fish1X = 1700;
 let y = 50;
 function setup() {
   bg = loadImage('assets/dinnerwithstrangers2.png');
+  fish1 = loadImage('assets/fish1.png');
   createCanvas(windowWidth, windowHeight);
   
 }
 
 function draw() {
   background(bg);
-  circle(800, y, 25);
+  fish1X -= 3;
+  image(fish1, fish1X, 800);
+  if (fish1X < -1000) {
+    fish1X = 1950;
+  }
  
 }
